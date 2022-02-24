89131fe14053bfe23cb5cc541f7d1e9d256135ff
JK <worudp12@gmail.com>
2022-02-25 04:13:40 +0900

    Chagefile_algorithm

--- algorithm/0224/[swea]4874_Fourth.py
+++ algorithm/0224/[swea]4874_Fourth.py
@@ -7,14 +7,14 @@ for t in range(1, T+1):
     stack = []
 
     for word in words:
-        if word == '+':
-            if len(stack) <= 1:
-                print('error')
+        if word == '+':           # + 연산자일 경우
+            if len(stack) <= 1:   # 피연산자가 부족하거나 스택이 비어있으면
+                print('error')    # 에러!
                 break
-            else:
-                stack.append(stack[-2] + stack[-1])
-                stack.pop(-2)
-                stack.pop(-2)
+            else:                 # 그렇지 않을 경우, 정상적으로 연산을 해서 스택에 저장
+                stack.append(stack[-2] + stack[-1]) 
+                stack.pop(-2)     # 연산에 사용한 피연산자 제거 
+                stack.pop(-2)     # 연산에 사용한 피연산자 제거 
         elif word == '-':
             if len(stack) <= 1:
                 print('error')
@@ -27,7 +27,7 @@ for t in range(1, T+1):
             if len(stack) <= 1:
                 print('error')
                 break
-            else:
+            else:                # /연산자일 경우,  항상 나누어떨어지기 때문에 //로 연산
                 stack.append(stack[-2] // stack[-1])
                 stack.pop(-2)
                 stack.pop(-2)
@@ -39,8 +39,8 @@ for t in range(1, T+1):
                 stack.append(stack[-2] * stack[-1])
                 stack.pop(-2)
                 stack.pop(-2)
-        elif word == '.':
-            if len(stack) > 1:
+        elif word == '.':       # 무조건 가장 마지막에 오는 토큰
+            if len(stack) > 1:  # 스택에 남아있는 피연산자가 2개 이상이면 에러!
                 print('error')
                 break
             else:
--- algorithm/0224/[swea]4880_토너먼트_카드게임.py
+++ algorithm/0224/[swea]4880_토너먼트_카드게임.py
@@ -2,18 +2,19 @@
 
 T = int(input())
 
+# 가위 바위 보 승리
 def battle(i, j):
-    if arr[i] == 1:
+    if arr[i] == 1:                  # 가위
         if arr[j] == 2:
             return j
         else:
             return i
-    elif arr[i] == 2:
+    elif arr[i] == 2:                # 바위
         if arr[j] == 3:
             return j
         else:
             return i
-    elif arr[i] == 3:
+    elif arr[i] == 3:                # 보
         if arr[j] == 1:
             return j
         else: return i
@@ -21,13 +22,13 @@ def battle(i, j):
 
 def check(start, end):
     # 종료 조건
-    if start == end:
-        return start
+    if start == end:                # 탐색이 끝나면
+        return start                # return
     else:
-        mid = (start + end) // 2
-        l = check(start, mid)
-        r = check(mid + 1, end)
-        return battle(l, r)
+        mid = (start + end) // 2    # 두 그룹으로 나누기 위함 
+        l = check(start, mid)       # 중간 값 기준 왼쪽 구간을 재귀
+        r = check(mid + 1, end)     # 중간 값 기준 오른쪽 구간 재귀
+        return battle(l, r)         # 재귀를 통해 얻은 l,r의 값을 battle함수의 매개변수로 활용
 
 for t in range(1, T+1):
     N = int(input())
